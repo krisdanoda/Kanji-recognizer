4ce544c79e0eda7b81e3d3a4d9e05becc7bcb063
SMerved <90383268+SMerved@users.noreply.github.com>
2023-05-17 14:16:16 +0200

    hjn

--- dataCleaning.py
+++ dataCleaning.py
@@ -5,7 +5,7 @@ def remove_min_occurences(labels, imgs):
     filtered_labels = [x for x in labels if count[x] > 100]
     filtered_images = []
     for r in range(0,len(labels)):
-        if(count[labels[r]]>100):
+        if count[labels[r]]>100:
             filtered_images.append(imgs[r])
     return filtered_labels, filtered_images
 
