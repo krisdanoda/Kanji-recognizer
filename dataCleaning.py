42cb91d64c8b59a5edc117c44463b1fa708d7d8e
SMerved <90383268+SMerved@users.noreply.github.com>
2023-05-17 14:54:37 +0200

    contour done maybe

--- dataCleaning.py
+++ dataCleaning.py
@@ -1,4 +1,5 @@
 from collections import Counter
+import cv2
 
 def remove_min_occurences(labels, imgs):
     count = Counter(labels)
@@ -9,4 +10,58 @@ def remove_min_occurences(labels, imgs):
             filtered_images.append(imgs[r])
     return filtered_labels, filtered_images
 
+def remove_by_contours(labels, images):
+    full_count = 0
+    image_contours = {}
+    filtered_images = []
+    filtered_labels = []
+    label = labels[0]
+    for i in range(0,len(images)):
+        count = 0
+        if label != labels[i]:
+            full_count = full_count/len(image_contours)
+            for k in image_contours.keys():
+                if image_contours[k] > full_count-1 and image_contours[k] < full_count+1:
+                    filtered_images.append(images[i])
+                    filtered_labels.append(labels[i])
+            full_count = 0
+            image_contours = {}
+        img = images[i]
+        ret, thresh = cv2.threshold(img, 127, 255, 0)
+        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
+        for c in contours:
+            if cv2.contourArea(c) > 50:
+                count+=1
+                full_count+=1
+        image_contours[i] = count
+        label = labels[i]
+    return filtered_labels, filtered_images
+
+def remove_by_contours2(labels, images):
+    full_count = 0
+    image_contours = {}
+    filtered_images = []
+    filtered_labels = []
+    label = labels[0]
+    for i in range(0,len(images)):
+        count = 0
+        if label != labels[i]:
+            full_count = full_count/len(image_contours)
+            for k in image_contours.keys():
+                if image_contours[k] < full_count-1 or image_contours[k] > full_count+1:
+                    filtered_images.append(images[i])
+                    filtered_labels.append(labels[i])
+            full_count = 0
+            image_contours = {}
+        img = images[i]
+        ret, thresh = cv2.threshold(img, 127, 255, 0)
+        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+        for c in contours:
+            if cv2.contourArea(c) > 50:
+                count+=1
+                full_count+=1
+        image_contours[i] = count
+        label = labels[i]
+    return filtered_labels, filtered_images
