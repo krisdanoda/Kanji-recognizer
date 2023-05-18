eac60a836e4f3198e0b219d818156b19c0ff1cf0
Sofiadoingcode <88271469+Sofiadoingcode@users.noreply.github.com>
2023-05-18 16:44:30 +0200

    setup the 2 options

--- input_image.py
+++ input_image.py
@@ -16,19 +16,14 @@ def give_image_meaning():
     input_images = pathlib.Path("input_images")
     list(input_images.iterdir())
 
-    input_images_strings = []
-    for item in input_images.iterdir():
-        input_images_strings.append(str(item))
-        print(f"{item} - {'dir' if item.is_dir() else 'file'}")
+    input_images_strings = [str(item) for item in input_images.iterdir()]
+    
 
     img = cv2.imread(input_images_strings[0])   # Read the image
-    print(type(img),img.shape)
-    plt.imshow(img)
+
 
     #RESIZE
     smallImg = helper_functions.resize(img, 64)
-    print(type(smallImg),smallImg.shape)
-    plt.imshow(smallImg)
 
     #SHAPE IMAGE
     new_image = cv2.cvtColor(smallImg, cv2.COLOR_RGB2GRAY)
@@ -43,17 +38,13 @@ def give_image_meaning():
     predictions = model.predict(dim_img)
     predicted_classes = predictions.argmax(axis=-1)
 
-    print('Predictions shape:', predictions.shape)
-    print('Predictions:', predictions)
-    print('Predicted class:', predicted_classes)
-    print(dim_img)
 
     #WEBSCRAPE MEANING
 
-    print(np.unique(Z)[predicted_classes][0])
-    print(helper_functions.to_kanji(np.unique(Z)[predicted_classes][0]))
-
-    webscraping.get_meaning(helper_functions.to_kanji(np.unique(Z)[predicted_classes][0]))
+    return_list = [helper_functions.to_kanji(np.unique(Z)[predicted_classes][0])]
+    return_list.append(webscraping.get_meaning(helper_functions.to_kanji(np.unique(Z)[predicted_classes][0])))
+    
+    return return_list
 
 
 
